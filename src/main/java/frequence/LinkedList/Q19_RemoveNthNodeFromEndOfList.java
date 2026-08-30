05a4159ddd771b07373e80315f9bb6a5a932867c
shuyu <anshuyu.123@163.com>
2026-08-31 01:21:31 +0800

    Add one-pass remove-nth-node solution

--- src/main/java/frequence/LinkedList/Q19_RemoveNthNodeFromEndOfList.java
+++ src/main/java/frequence/LinkedList/Q19_RemoveNthNodeFromEndOfList.java
@@ -12,6 +12,54 @@ package frequence.LinkedList;
 public class Q19_RemoveNthNodeFromEndOfList {
 
     /**
+     * 2026-08-31 进阶：dummy节点 + 固定距离快慢指针。
+     *
+     * <p>这个方法和下面先统计长度的实现，在渐进时间复杂度和指针总移动次数上没有本质区别：
+     * {@code fast}走完整条链表，{@code slow}还会走{@code len - n}个节点。所谓“一趟扫描”
+     * 更准确地理解为：使用一次连续遍历完成定位，不进行“统计长度后重新从头定位”的两个串行阶段。
+     *
+     * <p><b>固定距离：</b>
+     * <pre>{@code
+     * fast先走n步；
+     * 然后slow和fast一起走；
+     * fast到达尾节点时，slow到达正序第len-n个节点。
+     * }
+     * </pre>
+     * 要删除的是正序第{@code len-n+1}个节点，因此{@code slow}最终正好停在它的前驱
+     * {@code len-n}位置，不需要提前统计{@code len}。
+     *
+     * <p><b>dummy不改变倒数距离：</b>原链表的倒数第n个节点加上dummy后仍然是倒数第n个
+     * 真实节点；dummy只是在头节点之前增加了一个统一前驱，使删除原头节点也能执行同一句
+     * {@code slow.next = slow.next.next}。
+     *
+     * <p><b>变量注意：</b>下面使用{@code while (n-- > 0)}后，局部变量{@code n}会被修改；
+     * 上述公式中的n始终表示方法刚进入时的原始输入值。
+     */
+    public class SolutionOnePass20260831 {
+
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode fast = dummy;
+            ListNode slow = dummy;
+
+            while (n-- > 0) {
+                fast = fast.next;
+            }
+
+            // TODO: 【注意-易错】fast到达尾节点就应该停止，不能走到null。
+            // 此时slow从dummy出发移动了len-originalN次，正好停在待删除节点的前驱。
+            while (fast.next != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            slow.next = slow.next.next;
+            return dummy.next;
+        }
+    }
+
+    /**
      * 2026-08-31 我的两次遍历实现。
      *
      * <p><b>难点1：倒数位置转换为正序位置。</b>位置使用1-based：
