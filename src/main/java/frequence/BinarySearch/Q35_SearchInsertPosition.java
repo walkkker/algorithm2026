49e8d8716a16b03b0813878b74b9b6d09e5a14be
shuyu <anshuyu.123@163.com>
2026-09-12 23:20:21 +0800

    Record Q35 insertion boundary mistake

--- src/main/java/frequence/BinarySearch/Q35_SearchInsertPosition.java
+++ src/main/java/frequence/BinarySearch/Q35_SearchInsertPosition.java
@@ -11,6 +11,72 @@ package frequence.BinarySearch;
 public class Q35_SearchInsertPosition {
 
     /**
+     * 2026-09-12 复盘版本：普通闭区间二分结束后确定插入位置。
+     */
+    class SolutionReviewed20260912 {
+
+        /**
+         * TODO: 【致命错误】本题有一个致命错误，就是判断target不在数组中时，需要插入的位置。
+         * 如果target要插入到nums[m]的前面，那么target的插入下标其实是m，而不是m-1。
+         */
+        public int searchInsert(int[] nums, int target) {
+            int len = nums.length;
+            int left = 0;
+            int right = len - 1;
+            int mid = -1;
+
+            while (left <= right) {
+                mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            // TODO: 【超级错误】target与nums[mid]相比较，下列逻辑是错误的。
+            // 错误行：return nums[mid] < target ? mid + 1 : mid - 1;
+            // 如果nums[mid]>target，target应插入到nums[mid]前面，对应下标就是mid。
+            return nums[mid] < target ? mid + 1 : mid;
+        }
+    }
+
+    /**
+     * 2026-09-12 推荐写法：循环结束后直接返回left。
+     *
+     * <p>循环始终排除已经确定的位置。退出时{@code left == right + 1}，并且：
+     * <pre>
+     * [0, left)            所有元素都小于target
+     * [left, nums.length)  所有元素都大于target
+     * </pre>
+     * Q35保证元素互不相同且前面已经处理相等情况，因此left就是第一个大于target的位置，
+     * 也就是target应当插入的位置。这个结论不依赖最后一次mid，更稳定、更容易证明。
+     */
+    class SolutionReturnLeft20260912 {
+
+        public int searchInsert(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            // TODO: 【核心】插入位置是分界点left，不是mid的前一个位置。
+            return left;
+        }
+    }
+
+    /**
      TODO:【特别重要-适合所有题】【遗漏】ans==-1 的情况一定要考虑！！！ 这种你初始值，进循环，但是退出循环时 还保留初始值的情况，一定要在最后返回时 做分支检查！！！
      */
     class Solution {
