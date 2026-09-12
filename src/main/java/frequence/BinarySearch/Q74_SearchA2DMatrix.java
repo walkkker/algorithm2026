0de8c4b323c7ad0424c89cd16d54ac1de5d06eda
shuyu <anshuyu.123@163.com>
2026-09-12 23:49:22 +0800

    Add dated Q74 two-stage binary search review

--- src/main/java/frequence/BinarySearch/Q74_SearchA2DMatrix.java
+++ src/main/java/frequence/BinarySearch/Q74_SearchA2DMatrix.java
@@ -12,6 +12,62 @@ package frequence.BinarySearch;
 public class Q74_SearchA2DMatrix {
 
     /**
+     * 2026-09-12 错题复盘：先二分确定唯一候选行，再在该行执行普通二分。
+     *
+     * <p><b>本题核心：</b>
+     * <ol>
+     *     <li>基于{@code matrix[row][0]}寻找首元素小于等于target的最右一行；</li>
+     *     <li>在该候选行中执行普通二分，检查target是否存在。</li>
+     * </ol>
+     *
+     * <p>TODO: 【复杂度错误】线性遍历每一行再二分列的复杂度是
+     * {@code O(M + log N)}，不满足题目的{@code O(log(MN))}要求。当前代码对行和列都使用
+     * 二分，复杂度是{@code O(log M + log N) = O(log(MN))}，满足要求。
+     *
+     * <p>TODO: 【变量命名错误】矩阵行数已经使用m时，二分中点不要再使用m，应命名为mid，
+     * 避免同名或语义混淆。
+     */
+    class SolutionReviewed20260912 {
+
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int l = 0;
+            int r = m - 1;
+            int row = -1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (matrix[mid][0] == target) {
+                    return true;
+                } else if (matrix[mid][0] < target) {
+                    row = mid;
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+
+            if (row == -1) {
+                return false;
+            }
+
+            l = 0;
+            r = n - 1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (matrix[row][mid] == target) {
+                    return true;
+                } else if (matrix[row][mid] < target) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            return false;
+        }
+    }
+
+    /**
      二维映射一维 / 一维映射二维
      TODO： 一维坐标 映射 二维坐标规则(到处通用)： row=index/N;  col=index%N
      */
